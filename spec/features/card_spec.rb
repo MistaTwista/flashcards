2b59d0ea065c52ed10009063ab26401e58aeef53
Maxim <maxim@aeromultimedia.com>
2015-09-01 17:56:31 +0300

    code style fix

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
-describe Card, :type => :feature do
+describe Card, type: :feature do
   let!(:card) { create(:card) }
 
   it "card checking" do
     visit new_review_path
     within(".simple_form") do
-      fill_in 'review_translated_text', :with => 'deer'
+      fill_in 'review_translated_text', with: 'deer'
     end
     click_button "Check"
     expect(page).to have_content "Yes, you right!"
