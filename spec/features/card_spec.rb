da260e9d0697428d3dab83b5fb08c54b80d61ca7
Maxim <maxim@aeromultimedia.com>
2015-09-01 17:50:42 +0300

    card checking feature test

---
+++ spec/features/card_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Card, :type => :feature do
+  let!(:card) { create(:card) }
+
+  it "card checking" do
+    visit new_review_path
+    within(".simple_form") do
+      fill_in 'review_translated_text', :with => 'deer'
+    end
+    click_button "Check"
+    expect(page).to have_content "Yes, you right!"
+  end
+end
