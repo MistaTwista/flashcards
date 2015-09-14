8a4dfa407299c1ba00f7d1991d4541aa66fb0c2d
Maxim <maxim@aeromultimedia.com>
2015-09-14 16:24:20 +0300

    Codestyle fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,18 +36,18 @@ Rails.application.configure do
   # Raises helpful error messages.
   config.assets.raise_runtime_errors = true
 
-  #Paperclip
+  # Paperclip
   Paperclip.options[:command_path] = ENV["IMAGE_MAGICK_FOLDER"]
 
   config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
     },
-    :url => ':s3_domain_url',
-    :path => "/:class/:attachment/:id_partition/:style/:filename"
+    url: ':s3_domain_url',
+    path: "/:class/:attachment/:id_partition/:style/:filename"
   }
 
   # Raises error for missing translations
