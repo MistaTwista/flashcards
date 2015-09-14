806b21dd33f4c3c3c7f15977c7cf48b242eaf3e3
Maxim <maxim@aeromultimedia.com>
2015-09-14 15:12:35 +0300

    finals small fixes

--- spec/features/card_spec.rb
+++ spec/features/card_spec.rb
@@ -3,29 +3,21 @@ describe Card do
   let!(:card) { create(:card).update_attribute(:review_date, Date.today - 5.days) }
 
   it "right review" do
-    login
+    login_with("example@example.com", "123123")
     visit new_review_path
     within(".simple_form") do
       fill_in "review_translated_text", with: "deer"
     end
-    check
+    click_button "Check"
     expect(page).to have_content "Yes, you right!"
   end
 
   it "wrong review" do
-    login
+    login_with("example@example.com", "123123")
     within(".simple_form") do
       fill_in "review_translated_text", with: "diir"
     end
-    check
-    expect(page).to have_content "Nope, try again later!"
-  end
-
-  def login
-    login_with("example@example.com", "123123")
-  end
-
-  def check
     click_button "Check"
+    expect(page).to have_content "Nope, try again later!"
   end
 end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -2,13 +2,13 @@ require "rails_helper"
 
 describe User do
   it "create user" do
-    register
+    register_with("josh@wink.com", "DASFI@", "DASFI@")
     expect(page).to have_content "josh@wink.com"
   end
 
   it "existing user" do
-    register
-    register
+    register_with("josh@wink.com", "DASFI@", "DASFI@")
+    register_with("josh@wink.com", "DASFI@", "DASFI@")
     expect(page).to have_content "has already been taken"
   end
 
@@ -19,9 +19,5 @@ describe User do
     register_with("man@ohman.com", "123123", "123123")
     visit cards_path
     expect(page).to have_no_content "deer"
-  end
-
-  def register
-    register_with("josh@wink.com", "DASFI@", "DASFI@")
   end
 end
